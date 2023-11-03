157921a485d353c131733ae250dadb67f1dc8889
Jeremiah <j.agbaje@alustudent.com>
2023-11-03 15:56:38 -0700

    m

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-     import hidden_4
+    import hidden_4 as extra
 
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+
+for x in dir(extra):
+    if x[0:2] != "__":
+        print("{:s}".format(x))
