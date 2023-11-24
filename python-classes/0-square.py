ef96380b86469ad29de7efb3424fc960823ec764
Jeremiah <j.agbaje@alustudent.com>
2023-11-24 06:55:29 -0800

    add documentation

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
+"""
+THIS CODE CONTAINS NO MODULES
+"""
+
+
 class Square():
+    """
+    CLASS IS A SQUARE CLASS AND IS EMPTY
+    """
--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""
+THIS CODE CONTAINS NO MODULES
+"""
 
 
 class Square:
     def __init__(self, size):
         self._Square__size = size
+        """
+     SQUARE CLASS AND IS EMPTY
+    """
